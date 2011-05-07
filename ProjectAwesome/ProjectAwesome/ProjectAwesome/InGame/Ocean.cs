b551516da31c71b3f204bdd38522eb53fad97b27
Calvin Slusarski <smackcalvin@gmail.com>
2011-05-07 13:10:25 -0400

    adjusted scale of ocean, updated & removed old files...

--- ProjectAwesome/ProjectAwesome/ProjectAwesome/InGame/Ocean.cs
+++ ProjectAwesome/ProjectAwesome/ProjectAwesome/InGame/Ocean.cs
@@ -21,6 +21,7 @@ namespace ProjectAwesome
         {
             Position = new Vector2(START_POSITION_X, START_POSITION_Y);
             base.LoadContent(theContentManager, ASSETNAME);
+            sprite.Scale = .5f;
             // moving effect
             refractionEffect = theContentManager.Load<Effect>("refraction");
         }
